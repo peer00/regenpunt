3fa5b1c1b92ad1313d18aec5515baf582f5987b8
Joel Konijn <joelkonijn@Joels-Mac.lan>
2015-08-23 16:10:32 +0200

    toevoegen route api en tabel

--- assets/js/listeners.js
+++ assets/js/listeners.js
@@ -1,4 +1,5 @@
 var lopen;
+//var scope = {};
 
 $("#lopen").click(function(){
   lopen = 1;
@@ -15,3 +16,15 @@ $("#fietsen").click(function(){
 $(".routeInput .btn").click(function(){
     $(this).removeClass("btn-default").addClass("btn-primairy").siblings().removeClass("btn-primairy").addClass("btn-default");
 });
+
+var html = "<table style=\"width:100%\"><tr><th>Vertrekpunt</th><th>Bestemming</th><th>Aangemaakt</th></tr>";
+
+io.socket.get("/route/find", function(data, jwr){
+  console.log(data);
+  for (var i = 0; i < data.length; i++) {
+    html += "<tr><td>" + data[i].origin + "</td><td>" + data[i].destination + "</td><td><time>" + Date(data[i].createdAt) + "</time></td></tr>"
+  };
+
+  $("#listAllRoutes").append(html);
+
+})
